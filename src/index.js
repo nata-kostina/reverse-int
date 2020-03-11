ce0c1911e20d4630cad7acd43b557b2bd7ce8f72
nata-kostina <natakostina123@gmail.com>
2020-03-11 17:07:20 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let res = 0;
+    n=Math.abs(n);
+    while (n > 0) {
+        res = res * 10 + n % 10;
+        n = Math.trunc(n / 10);
+    }
+    return res;
 }
